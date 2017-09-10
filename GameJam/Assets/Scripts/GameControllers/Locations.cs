61ee290465c08933bc74d472c61a4c7af191b5c2
Nancy McCollough <spacecolossi@gmail.com>
2017-09-10 11:38:50 -0700

    location base

---
+++ GameJam/Assets/Scripts/GameControllers/Locations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locations {
+
+  //
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
+public class Location {
+
+  private string name;
+  private string description;
+  private int id; 
+  // array of next locations
+
+  public Location(string name, string description) {
+
+  }
+
+
+}
